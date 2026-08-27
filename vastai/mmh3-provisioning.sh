1ed7fd6164179928112a974c315abaccbdabd707
huchukato <huchukato@gmail.com>
2026-08-27 14:46:25 +0200

    Remove Crystools-MonitorOnly from provisioning and README
    The node does not work in the current setup. Removed from Dockerfile,
    Vast.ai provisioning, and RunPod README custom node list.

--- vastai/mmh3-provisioning.sh
+++ vastai/mmh3-provisioning.sh
@@ -19,7 +19,6 @@ PIP_PACKAGES=(
 NODES=(
     "https://github.com/huchukato/comfy-tagcomplete"
     "https://github.com/huchukato/ComfyUI-QwenVL-Mod"
-    "https://github.com/BobRandomNumber/ComfyUI-Crystools-MonitorOnly"
     "https://github.com/Larryvrh/ComfyUI-MiniMax-H3-Turbo"
     "https://github.com/huchukato/ComfyUI-RIFE-TensorRT-Auto"
     "https://github.com/huchukato/ComfyUI-Upscaler-TensorRT-Auto"
